8fef21124727c06589a7957eee4a113be89714b9
Franciscoua <Francisco.ua.1989@gmail.com>
2023-06-05 20:23:59 +0200

    Asi se va a quedar hasta los 4000

--- cromannumbers_clase.py
+++ cromannumbers_clase.py
@@ -13,17 +13,6 @@ class RomanNumber:
         else:
             raise RomanNumber("Debe inicializarse con un entero o romano valido")
 
-    def __add__(self, otro_numero):
-        resultado = self.numero + otro_numero.numero
-        if resultado <0:
-            raise RomanNumberError("No existen los números negativos en romano")
-        return RomanNumber(resultado)
-
-    def __sub__(self, otro_numero):
-        resultado = self.numero - otro_numero.numero
-        if resultado <0:
-            raise RomanNumberError("No existen los números negativos en romano")
-        return RomanNumber(resultado)
 
     @property
     def numero(self):
@@ -48,11 +37,42 @@ class RomanNumber:
     """
     
     def __eq__(self, other):
+        other = self.__to_roman(other)
         return self.numero == other.numero
+    
+    def __req__(self,other):
+        return self.__eq__(other)
+    
+    def __lt__(self,other):
+        other = self.__to_roman(other)
+        return self.numero<other.numero
+    
+    def __le__(self,other):
+        other = self.__to_roman(other)
+        return self.numero<=other.numero
+    
+    def __gt__(self,other):
+        other = self.__to_roman(other)
+        return self.numero>other.numero
+    
+    def __ge__(self,other):
+        other = self.__to_roman(other)
+        return self.numero>=other.numero
+    
+    def __ne__(self,other):
+        other = self.__to_roman(other)
+        return self.numero!=other.numero
+    
+
     
     """
     metodo mágico para aritmética
+
     """
+    def __to_roman(self,otro):
+        if not isinstance(otro,RomanNumber):
+            otro = RomanNumber(otro)
+        return otro
 
     def __mul__(self, otro):
         if  not isinstance(otro, RomanNumber):
@@ -65,6 +85,55 @@ class RomanNumber:
     def __rmul__(self, otro):
         return self.__mul__(otro)
     
+
+    
+    def __add__(self, otro2):
+        if not isinstance(otro2, RomanNumber):
+            otro2 = RomanNumber(otro2)
+        resultado = self.numero + otro2.numero
+        if resultado <0:
+            raise RomanNumberError("No existen los números negativos en romano")
+        return RomanNumber(resultado)
+    
+    def __radd__(self, otro2):
+        return self.__add__(otro2)
+    
+    
+
+    def __sub__(self, otro3):
+        if not isinstance(otro3, RomanNumber):
+            otro3 = RomanNumber(otro3)
+            
+        return RomanNumber(self.numero - otro3.numero)
+
+
+    def __rsub__(self, otro):
+        otro = self.__to_roman(otro)
+
+        return otro.__sub__(self)
+
+    
+    
+    def __floordiv__(self, otro4):
+        if not isinstance(otro4, RomanNumber):
+            otro4 = RomanNumber(otro4)
+        resultado = self.numero//otro4.numero
+        return RomanNumber(resultado)
+
+    
+    def __rfloordiv__(self, otro4):
+        otro4 = RomanNumber(otro4) 
+        return otro4.__floordiv__(self)
+    
+    def __pow__(self, otro):
+        otro = self.__to_roman(otro)
+        return RomanNumber(self.numero ** otro.numero)
+    
+    def __rpow__(self, otro):
+        otro = self.__to_roman(otro)
+        return otro.__pow__(self)
+    
+
     """
     metodo mágico para representación
     """
--- test_class_romanumbers.py
+++ test_class_romanumbers.py
@@ -42,3 +42,73 @@ def test_multiplicacion():
 
 
     assert 5* romano1 == RomanNumber(50)
+
+def test_suma():
+    romano1 = RomanNumber(5)
+    romano2 = RomanNumber("X")
+    assert romano1 + romano2 == RomanNumber("XV")
+    assert romano1 +5 == RomanNumber(10)
+    assert romano1 + "V" == RomanNumber(10)
+
+    assert 5+ romano1 == RomanNumber(10)
+
+def test_resta():
+    romano1 = RomanNumber("X")
+    romano2 = RomanNumber(5)
+    assert romano1 - romano2 == RomanNumber("V")
+    assert romano1 - 5 == RomanNumber(5)
+    assert romano1 - "V" == RomanNumber(5)
+    assert 10- romano2 == RomanNumber(5)
+
+    romano3 = RomanNumber(10)
+    romano4 = RomanNumber("V")
+    assert romano3 - romano4 == RomanNumber("V")
+    assert romano3 - 5 == RomanNumber(5)
+    assert romano3 - "V" == RomanNumber(5)
+
+
+
+def test_division():
+    romano1 = RomanNumber("X")
+    
+    assert romano1 // 5 == RomanNumber("II")
+    assert romano1 // 5 == RomanNumber(2)
+    assert romano1 // "V" == RomanNumber(2)
+
+    assert 5// romano1 == RomanNumber(0)
+    
+def test_operaciones_matematicas():
+    romano1 = RomanNumber("X")
+
+    assert romano1 + 5 == RomanNumber(15)
+    assert 5 + romano1 == RomanNumber(15)
+    
+
+    assert romano1 - 5 == RomanNumber(5)
+    assert 15 -romano1 == RomanNumber(5)
+
+    assert romano1 ** 2 == RomanNumber(100)
+    assert 2 ** romano1 == RomanNumber(1024)
+
+def test_operaciones_logicas():
+    romano1 = RomanNumber(10)
+    assert romano1 == 10
+    assert 10 == romano1
+
+    assert romano1 > 9
+    assert 9 < romano1
+    assert romano1 >= 9
+    assert 9 <= romano1
+    assert romano1 >= 10
+    assert 10 <= romano1
+
+    assert romano1 < 11
+    assert 11 > romano1
+    assert romano1 <= 10
+    assert 10 >= romano1
+    assert romano1 <= 11
+    assert 11 >= romano1
+
+
+    assert romano1 != 11
+    assert 11 != romano1
